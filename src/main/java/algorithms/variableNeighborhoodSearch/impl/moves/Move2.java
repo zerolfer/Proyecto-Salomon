73ef734c193cda101b07cc5bd62495c3741956f8
Sergio Flórez <sergiozerolf@gmail.com>
2019-04-05 13:57:10 +0200

    (arreglo para que compile el commit anterior...)

--- src/main/java/algorithms/variableNeighborhoodSearch/impl/moves/Move2.java
+++ src/main/java/algorithms/variableNeighborhoodSearch/impl/moves/Move2.java
@@ -1,6 +1,21 @@
 package algorithms.variableNeighborhoodSearch.impl.moves;
 
 import algorithms.variableNeighborhoodSearch.NeighborStructure;
+import estructurasDatos.Solucion;
 
-public class Move2 extends NeighborStructure {
+public class Move2 extends AbstractNeighborStructure {
+    @Override
+    public Solucion bestImprovement(Solucion solucion) {
+        return null;
+    }
+
+    @Override
+    public Solucion FirstImprovement(Solucion solucion) {
+        return null;
+    }
+
+    @Override
+    public Solucion Shake(Solucion solucion) {
+        return null;
+    }
 }
